f8644907ff151d53292a07fe3e5be58738baf0f0
Maksim Kutishchev <max@amopro.ru>
2021-03-08 17:00:49 +0400

    update banner

--- build/webpack-configs/banner.js
+++ build/webpack-configs/banner.js
@@ -1,7 +1,9 @@
 const {version, homepage} = require('../../package');
+const year = new Date().getFullYear();
+const years = year === 2021 ? '2021' : '2021-' + year;
 
 module.exports = `
-vue-diagram-editor v${version} | (c) 2021-${new Date().getFullYear()} Maksim Kutishchev
+vue-diagram-editor v${version} | (c) ${years} Maksim Kutishchev
 Released under the Apache 2 License.
 ${homepage}
 `.trim();
